6c862ac47daf3db94204564d50d8bb50e8abb1f7
Hazem Abbassi <ha.zem.abbassi6@gmail.com>
2023-03-02 22:41:27 +0100

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <div class="page-header clear-filter" filter-color="orange">
+      <parallax
+        class="page-header-image"
+        style="background-image:url('img/Cybersecurity.jpg')"
+      >
+      </parallax>
+      <div class="container">
+        <div class="content-center brand">
+          <img class="n-logo" src="img/Logo_Security.png" alt="" />
+          <h1 class="h1-seo">SOC platform</h1>
+          <h3>we provide security with the necessary tools to monitor, detect, and respond to cyber threats in real-time.</h3>
+        </div>
+        <h6 class="category category-absolute">
+          Designed by
+          <a href="https://www.defensy.io/" target="_blank">
+            <img src="img/DefensyLogo.png" class="invision-logo" /> </a
+          >
+          <!-- . Coded by
+          <a href="https://www.creative-tim.com" target="_blank">
+            <img
+              src="img/creative-tim-white-slim2.png"
+              class="creative-tim-logo"
+            /> </a
+          >. -->
+        </h6>
+      </div>
+    </div>
+    <div class="main">
+      <div class="section section-images">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="hero-images-container">
+                <img src="img/hero-image-1.png" alt="" />
+              </div>
+              <div class="hero-images-container-1">
+                <img src="img/hero-image-2.png" alt="" />
+              </div>
+              <div class="hero-images-container-2">
+                <img src="img/hero-image-3.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <basic-elements></basic-elements>
+    <navigation></navigation>
+    <tabs-section></tabs-section>
+    <progress-pagination></progress-pagination>
+    <notifications></notifications>
+    <typography></typography>
+    <javascript-components></javascript-components>
+    <carousel-section></carousel-section>
+    <nucleo-icons-section></nucleo-icons-section> -->
+    <div class="section">
+      <div class="container text-center">
+        <div class="row justify-content-md-center">
+          <div class="col-md-12 col-lg-12">
+            <h2 class="title">Select Your Service Plan</h2>
+            <Plans />
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- <signup-form></signup-form>
+    <examples-section></examples-section>
+    <download-section></download-section> -->
+  </div>
+</template>
+<script>
+import { Parallax } from '@/components';
+import BasicElements from './components/BasicElementsSection';
+import Navigation from './components/Navigation';
+import TabsSection from './components/Tabs';
+import ProgressPagination from './components/ProgressPagination';
+import Notifications from './components/Notifications';
+import Typography from './components/Typography';
+import JavascriptComponents from './components/JavascriptComponents';
+import CarouselSection from './components/CarouselSection';
+import NucleoIconsSection from './components/NucleoIconsSection';
+import SignupForm from './components/SignupForm';
+import ExamplesSection from './components/ExamplesSection';
+import DownloadSection from './components/DownloadSection';
+import Plans from './components/Plans';
+
+export default {
+  name: 'index',
+  bodyClass: 'index-page',
+  components: {
+    // Parallax,
+    // BasicElements,
+    // Navigation,
+    // TabsSection,
+    // ProgressPagination,
+    // Notifications,
+    // Typography,
+    // JavascriptComponents,
+    // CarouselSection,
+    // NucleoIconsSection,
+    // SignupForm,
+    // ExamplesSection,
+    // DownloadSection
+    Plans
+  }
+};
+</script>
+<style></style>
