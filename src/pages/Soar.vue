8a471c3740a94f8c17ba9a7d104e40ee4dce100f
MonTa <39422192+SpawN1337@users.noreply.github.com>
2023-03-18 02:24:38 +0100

    completed soar still toggle and navigation

--- src/pages/Soar.vue
+++ src/pages/Soar.vue
@@ -1,73 +1,186 @@
 <template>
   <div>
     <div class="page-header clear-filter" filter-color="orange">
-      <parallax
+      <div
         class="page-header-image"
         style="background-image: url('img/bg3.jpg')"
-      >
-      </parallax>
-
-      <div class="typer">
-            <span class="typed-text">{{ typeValue }}</span>
-            <span class="blinking-cursor">|</span>
+      ></div>
+      <br />
+      <div class="content-center row">
+        <div class="colcol-6">
+          <img height="250" src="img/soar.png" alt="" />
+        </div>
+        <div class="col-6">
+          <br />
+          <h2 style="font-weight: bold">Notifications.</h2>
+          NIST CSF
+          <span class="id-color-2">RS.CO-3:</span>
+          Information is shared consistent with response plans. <br />
+          <div>
+            <button href="middle" type="button" class="btn btn-primary">
+              Show me more
+            </button>
           </div>
+        </div>
+        <div class="col-6">
+          <h2 style="font-weight: bold">Workflows and Orchestration.</h2>
+          NIST CSF
+          <span class="id-color-2">RS.CO-2:</span>
+          Incidents are reported consistent with established criteria.
+          <div>
+            <button type="button" class="btn btn-primary">Show me more</button>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="main">
-      <div class="section section-images">
+    <br />
+    <div>
+      <div class="position-relative section-images">
         <div class="container">
-          <div class="row">
-            <div class="col-md-12">
-              <div class="hero-images-container">
-                <img src="img/hero-image-1.png" alt="" />
-              </div>
-              <div class="hero-images-container-1">
-                <img src="img/hero-image-2.png" alt="" />
-              </div>
-              <div class="hero-images-container-2">
-                <img src="img/hero-image-3.png" alt="" />
-              </div>
+          <br />
+          <div class="row justify-content-center">
+            <div class="hero-images-container">
+              <br />
+              <img src="img/laptop.png" alt="" />
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-   
-    <div class="section">
-      <div class="container text-center">
-        <div class="row justify-content-md-center">
-          <div class="col-md-12 col-lg-12">
-            <h2 class="title">Select Your Service Plan</h2>
-            <Plans />
-          </div>
-        </div>
-      </div>
-    </div>
 
-    <!-- <signup-form></signup-form>
-      <examples-section></examples-section>
-      <download-section></download-section> -->
+            <ul
+              class="position-relative list-group list-group-horizontal-sm row"
+            >
+              <li class="col-md-3">
+                <div id="middle" class="stat-card">
+                  <a href="#middle"></a>
+                  <div class="stat-icon right">
+                    <span class="bg-measure house"></span><i class="house"></i>
+                  </div>
+                  <h2 style="font-weight: bold">Workflows.</h2>
+                  <br />
+                  <h4 class="stat-num"></h4>
+                  <div class="subtext">
+                    <i class="glyphicon glyphicon-triangle-top txt-ontrack"></i>
+                    Workflows are made using a varitety of frameworks and tools.
+                  </div>
+                </div>
+              </li>
+
+              <li class="col-md-3">
+                <div class="stat-card">
+                  <div class="stat-icon right">
+                    <span class="bg-measure users_single-02"></span
+                    ><i class="glyphicon glyphicon-home"></i>
+                  </div>
+                  <h2 style="font-weight: bold">
+                    Orchestration and Playbooks.
+                  </h2>
+                  <h4 class="stat-num"></h4>
+                  <div class="subtext">
+                    <i class="glyphicon glyphicon-triangle-top txt-ontrack"></i>
+                    SOCFortress crafts playbooks to orchestrate full response
+                    chains.
+                  </div>
+                </div>
+              </li>
+              <li class="col-md-3">
+                <div class="stat-card">
+                  <div class="stat-icon right">
+                    <span class="bg-measure users_single-02"></span
+                    ><i class="glyphicon glyphicon-home"></i>
+                  </div>
+                  <h2 style="font-weight: bold">Orchestrated Response.</h2>
+                  <h4 class="stat-num"></h4>
+                  <div class="subtext">
+                    <i class="glyphicon glyphicon-triangle-top txt-ontrack"></i>
+                    Playbooks integrate different actions and targets.
+                  </div>
+                </div>
+              </li>
+              <li class="col-md-3">
+                <div class="stat-card">
+                  <div class="stat-icon right">
+                    <span class="bg-measure users_single-02"></span
+                    ><i class="glyphicon glyphicon-home"></i>
+                  </div>
+                  <h2 style="font-weight: bold">Artifacts Collection.</h2>
+                  <h4 class="stat-num"></h4>
+                  <div class="subtext">
+                    <i class="glyphicon glyphicon-triangle-top txt-ontrack"></i>
+                    Playbooks execution can extend beyond the affected asset,
+                    collecting additional artifacts.
+                  </div>
+                </div>
+              </li>
+              <li class="col-md-3">
+                <div class="stat-card">
+                  <div class="stat-icon right">
+                    <span class="bg-measure users_single-02"></span
+                    ><i class="glyphicon glyphicon-home"></i>
+                  </div>
+                  <h2 style="font-weight: bold">User Notifications.</h2>
+                  <h4 class="stat-num"></h4>
+                  <div class="subtext">
+                    <i class="glyphicon glyphicon-triangle-top txt-ontrack"></i>
+                    Different communication channels available as part of the
+                    orchestrated response.
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div style="padding-top: 100px" class="d-grid gap-3">
+        <div id="app">
+          <button
+            @click="toggle1 = !toggle1"
+            type="button"
+            class="btn btn-secondary btn-lg btn-block block"
+          >
+            Plugins and Integrations.
+          </button>
+          <div v-if="toggle1">
+            <img class="show" height="250" src="img/soar1.png" alt="" />
+          </div>
+        </div>
+        <div>
+          <button
+            @click="toggle2 = !toggle2"
+            type="button"
+            class="btn btn-secondary btn-lg btn-block block"
+          >
+            Workflows.
+          </button>
+          <div v-show="toggle2">
+            <img class="show" src="img/soar2.png" alt="" />
+          </div>
+        </div>
+        <div>
+          <button
+            @click="toggle3 = !toggle3"
+            type="button"
+            class="btn btn-secondary btn-lg btn-block block"
+          >
+            Apps Ecosystem.
+          </button>
+          <div v-show="toggle3">
+            <img class="show" height="250" src="img/soar3.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-import { Parallax } from "@/components";
-import BasicElements from "./components/BasicElementsSection";
-import Navigation from "./components/Navigation";
-import TabsSection from "./components/Tabs";
-import ProgressPagination from "./components/ProgressPagination";
-import Notifications from "./components/Notifications";
-import Typography from "./components/Typography";
-import JavascriptComponents from "./components/JavascriptComponents";
-import CarouselSection from "./components/CarouselSection";
-import NucleoIconsSection from "./components/NucleoIconsSection";
-import SignupForm from "./components/SignupForm";
-import ExamplesSection from "./components/ExamplesSection";
-import DownloadSection from "./components/DownloadSection";
-import Plans from "./components/Plans";
-import carousel from "vue-owl-carousel";
-
+import Button from "../components/Button.vue";
 export default {
   name: "index",
   //data for typewriter
+  setup() {
+    return {
+      toggle1: false,
+      toggle2: true,
+      toggle3: true,
+    };
+  },
   data: () => {
     return {
       typeValue: "",
@@ -119,26 +232,123 @@ export default {
   },
   // till here typewriter
   bodyClass: "index-page",
-  components: {
-    // Parallax,
-    // BasicElements,
-    // Navigation,
-    // TabsSection,
-    // ProgressPagination,
-    // Notifications,
-    // Typography,
-    // JavascriptComponents,
-    // CarouselSection,
-    // NucleoIconsSection,
-    // SignupForm,
-    // ExamplesSection,
-    // DownloadSection
-    Plans,
-    //   carousel
-  },
 };
 </script>
 <style lang="scss" scoped>
+.show {
+  width: 75%;
+  height: 450px;
+  background-repeat: no-repeat;
+  background-size: contain;
+  margin-left: 12%;
+  margin-bottom: 50px;
+}
+.block {
+  margin-left: 12%;
+  display: block;margin-bottom: 20px;;
+  width: 75%;
+  justify-content: center;
+  border: none;
+  background-color: #0446aa;
+  padding: 14px 28px;
+  font-size: 16px;
+  cursor: pointer;
+  text-align: center;
+}
+ul {
+  list-style-type: none;
+}
+h2.headings {
+  margin-bottom: 20px;
+}
+.stat-card h3 {
+  font-size: 16px;
+  margin-top: 5px;
+  line-height: 20px;
+  padding-right: 45px;
+  color: #333;
+  text-align: left;
+}
+.stat-card {
+  position: relative;
+  display: inline-block;
+  background-color: #fff;
+  width: 320px;
+  min-height: 170px;
+  text-align: left;
+  padding: 15px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: rgb(204, 204, 204);
+  border-image: initial;
+  overflow: hidden;
+}
+.stat-icon {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  width: 60px;
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+}
+.stat-icon.right {
+  right: 0px;
+}
+.stat-icon span {
+  position: absolute;
+  z-index: 8;
+  top: -40px;
+  left: 5px;
+  width: 100px;
+  height: 100px;
+  line-height: 120px;
+  border-radius: 50%;
+}
+.stat-icon i {
+  padding-left: 15px;
+  color: rgb(255, 255, 255);
+  z-index: 9;
+  font-size: 24px;
+}
+.bg-target {
+  background-color: #e07000;
+}
+.txt-target {
+  color: #e07000;
+}
+.bg-ontrack {
+  background-color: #007613;
+}
+.txt-ontrack {
+  color: #007613;
+}
+.bg-failing {
+  background-color: #d43e3a;
+}
+.txt-failing {
+  color: #d43e3a;
+}
+.bg-measure {
+  background-color: #186ea5;
+}
+.txt-measure {
+  color: #186ea5;
+}
+.stat-card h4.stat-num {
+  font-size: 45px;
+  text-align: left;
+}
+.stat-card .subtext {
+  font-size: 15px;
+}
+.stat-card .source {
+  font-size: 12px;
+  margin-top: 15px;
+}
+.stat-card .open-btn {
+  margin-top: 15px;
+}
 .typerclass {
   margin-bottom: 0px;
   font-weight: bold;
