ed615ad6185ab59eebeb6d62353f32c7880635fb
MonTa <39422192+SpawN1337@users.noreply.github.com>
2023-03-18 20:32:31 +0100

    fixed pics in soar

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,10 +3,10 @@
     <div class="page-header clear-filter" filter-color="orange">
       <parallax
         class="page-header-image"
-        style="background-image:url('img/Cybersecurity.jpg')"
+        style="background-image: url('img/Cybersecurity.jpg')"
       >
       </parallax>
-      <div >
+      <div>
         <div class="content-center brand">
           <img class="n-logo" src="img/Logo_Security.png" alt="" />
           <h2 class="typerclass">SOC platform</h2>
@@ -14,25 +14,27 @@
             <span class="typed-text">{{ typeValue }}</span>
             <span class="blinking-cursor">|</span>
           </div>
-          <h3>we provide security with the necessary tools to monitor, detect, and respond to cyber threats in real-time.</h3>
+          <h3>
+            we provide security with the necessary tools to monitor, detect, and
+            respond to cyber threats in real-time.
+          </h3>
         </div>
         <div class="typer">
-        <h6 class="category category-absolute">
-          Designed by
-          <a href="https://www.defensy.io/" target="_blank">
-            <img src="img/DefensyLogoW.png" class="img " /> </a
-          >
-          <!-- . Coded by
+          <h6 class="category category-absolute">
+            Designed by
+            <a href="https://www.defensy.io/" target="_blank">
+              <img src="img/DefensyLogoW.png" class="img" />
+            </a>
+            <!-- . Coded by
           <a href="https://www.creative-tim.com" target="_blank">
             <img
               src="img/creative-tim-white-slim2.png"
               class="creative-tim-logo"
             /> </a
           >. -->
-        </h6>
-        
+          </h6>
+        </div>
       </div>
-    </div>
     </div>
     <div class="main">
       <div class="section section-images">
@@ -62,6 +64,111 @@
     <javascript-components></javascript-components>
     <carousel-section></carousel-section>
     <nucleo-icons-section></nucleo-icons-section> -->
+    <carousel
+      :items="4"
+      :margin="10"
+      :autoplay="true"
+      :mouseDrag="true"
+      :stagePadding="50"
+      :dots="true"
+      :autoplayHoverPause="true"
+    >
+      <div class="slider-card">
+        <div class="d-flex justify-content-center align-items-center mb-4">
+          <img src="../../src/assets/images/slide-1.jpg" alt="" />
+        </div>
+        <h5 class="mb-0 text-center"><b>Threat intelligence</b></h5>
+        <p class="text-center p-4">
+          A SOC should have access to the latest threat intelligence data, which
+          can help identify potential security threats and vulnerabilities.
+        </p>
+      </div>
+      <div class="slider-card">
+        <div class="d-flex justify-content-center align-items-center mb-4">
+          <img src="../../src/assets/images/slide-2.jpg" alt="" />
+        </div>
+        <h5 class="mb-0 text-center">
+          <b>Security information and event management (SIEM)</b>
+        </h5>
+        <p class="text-center p-4">
+          A SIEM system is used to collect and analyze security event data from
+          various sources within the organization's network. This can help
+          detect security incidents in real-time and provide insights for
+          improving security posture.
+        </p>
+      </div>
+      <div class="slider-card">
+        <div class="d-flex justify-content-center align-items-center mb-4">
+          <img src="../../src/assets/images/slide-3.jpg" alt="" />
+        </div>
+        <h5 class="mb-0 text-center"><b>Incident response</b></h5>
+        <p class="text-center p-4">
+          A SOC should have a documented incident response plan in place that
+          outlines the steps that should be taken in the event of a security
+          breach or incident. This includes procedures for containment,
+          investigation, and recovery.
+        </p>
+      </div>
+      <div class="slider-card">
+        <div class="d-flex justify-content-center align-items-center mb-4">
+          <img src="../../src/assets/images/slide-4.jpg" alt="" />
+        </div>
+        <h5 class="mb-0 text-center"><b>Monitoring and analysis</b></h5>
+        <p class="text-center p-4">
+          The SOC should have a team of analysts who are responsible for
+          monitoring the network for potential security threats and analyzing
+          security event data to identify patterns and trends.
+        </p>
+      </div>
+      <div class="slider-card">
+        <div class="d-flex justify-content-center align-items-center mb-4">
+          <img src="../../src/assets/images/slide-5.jpg" alt="" />
+        </div>
+        <h5 class="mb-0 text-center"><b>Threat hunting</b></h5>
+        <p class="text-center p-4">
+          The SOC team should also proactively search for potential security
+          threats that may not have been detected by automated monitoring
+          systems.
+        </p>
+      </div>
+      <div class="slider-card">
+        <div class="d-flex justify-content-center align-items-center mb-4">
+          <img src="../../src/assets/images/slide-6.jpg" alt="" />
+        </div>
+        <h5 class="mb-0 text-center"><b>Forensics and investigations</b></h5>
+        <p class="text-center p-4">
+          If a security breach does occur, the SOC should have the tools and
+          expertise necessary to conduct a forensic investigation to determine
+          the cause and scope of the breach.
+        </p>
+      </div>
+      <div class="slider-card">
+        <div class="d-flex justify-content-center align-items-center mb-4">
+          <img src="../../src/assets/images/slide-1.jpg" alt="" />
+        </div>
+        <h5 class="mb-0 text-center"><b>Communication and reporting</b></h5>
+        <p class="text-center p-4">
+          The SOC should communicate with stakeholders across the organization
+          to keep them informed about security threats and incidents. They
+          should also provide regular reports on security metrics and trends to
+          senior management.
+        </p>
+      </div>
+      <div class="slider-card">
+        <div class="d-flex justify-content-center align-items-center mb-4">
+          <img src="../../src/assets/images/slide-2.jpg" alt="" />
+        </div>
+        <h5 class="mb-0 text-center"><b>Continuous improvement</b></h5>
+        <p class="text-center p-4">
+          The SOC should be committed to continuous improvement, regularly
+          reviewing and updating their processes, procedures, and technologies
+          to stay ahead of evolving security threats.
+        </p>
+      </div>
+    </carousel>
+    <!-- <signup-form></signup-form>
+    <examples-section></examples-section>
+    <download-section></download-section> -->
     <div class="section">
       <div class="container text-center">
         <div class="row justify-content-md-center">
@@ -72,95 +179,34 @@
         </div>
       </div>
     </div>
-    <carousel :items="4" :margin="10" :autoplay="true" :mouseDrag="true" :stagePadding="50" :dots="true" :autoplayHoverPause="true">
-          <div class="slider-card">
-						<div class="d-flex justify-content-center align-items-center mb-4">
-							<img src="../../src/assets/images/slide-1.jpg" alt="" >
-						</div>
-						<h5 class="mb-0 text-center"><b>Threat intelligence</b></h5>
-						<p class="text-center p-4">A SOC should have access to the latest threat intelligence data, which can help identify potential security threats and vulnerabilities.</p>
-					</div>
-					<div class="slider-card">
-						<div class="d-flex justify-content-center align-items-center mb-4">
-							<img src="../../src/assets/images/slide-2.jpg" alt="">
-						</div>
-						<h5 class="mb-0 text-center"><b>Security information and event management (SIEM)</b></h5>
-						<p class="text-center p-4">A SIEM system is used to collect and analyze security event data from various sources within the organization's network. This can help detect security incidents in real-time and provide insights for improving security posture.</p>
-					</div>
-					<div class="slider-card">
-						<div class="d-flex justify-content-center align-items-center mb-4">
-							<img src="../../src/assets/images/slide-3.jpg" alt="">
-						</div>
-						<h5 class="mb-0 text-center"><b>Incident response</b></h5>
-						<p class="text-center p-4">A SOC should have a documented incident response plan in place that outlines the steps that should be taken in the event of a security breach or incident. This includes procedures for containment, investigation, and recovery.</p>
-					</div>
-					<div class="slider-card">
-						<div class="d-flex justify-content-center align-items-center mb-4">
-							<img src="../../src/assets/images/slide-4.jpg" alt="">
-						</div>
-						<h5 class="mb-0 text-center"><b>Monitoring and analysis</b></h5>
-						<p class="text-center p-4">The SOC should have a team of analysts who are responsible for monitoring the network for potential security threats and analyzing security event data to identify patterns and trends.</p>
-					</div>
-          <div class="slider-card">
-						<div class="d-flex justify-content-center align-items-center mb-4">
-							<img src="../../src/assets/images/slide-5.jpg" alt="">
-						</div>
-						<h5 class="mb-0 text-center"><b>Threat hunting</b></h5>
-						<p class="text-center p-4">The SOC team should also proactively search for potential security threats that may not have been detected by automated monitoring systems.</p>
-					</div>
-					<div class="slider-card">
-						<div class="d-flex justify-content-center align-items-center mb-4">
-							<img src="../../src/assets/images/slide-6.jpg" alt="">
-						</div>
-						<h5 class="mb-0 text-center"><b>Forensics and investigations</b></h5>
-						<p class="text-center p-4"> If a security breach does occur, the SOC should have the tools and expertise necessary to conduct a forensic investigation to determine the cause and scope of the breach.</p>
-					</div>
-          <div class="slider-card">
-						<div class="d-flex justify-content-center align-items-center mb-4">
-							<img src="../../src/assets/images/slide-1.jpg" alt="">
-						</div>
-						<h5 class="mb-0 text-center"><b>Communication and reporting</b></h5>
-						<p class="text-center p-4">The SOC should communicate with stakeholders across the organization to keep them informed about security threats and incidents. They should also provide regular reports on security metrics and trends to senior management.</p>
-					</div>
-          <div class="slider-card">
-						<div class="d-flex justify-content-center align-items-center mb-4">
-							<img src="../../src/assets/images/slide-2.jpg" alt="">
-						</div>
-						<h5 class="mb-0 text-center"><b>Continuous improvement</b></h5>
-						<p class="text-center p-4">The SOC should be committed to continuous improvement, regularly reviewing and updating their processes, procedures, and technologies to stay ahead of evolving security threats.</p>
-					</div>
-          
-</carousel>
-    <!-- <signup-form></signup-form>
-    <examples-section></examples-section>
-    <download-section></download-section> -->
   </div>
+  
 </template>
 <script>
-import { Parallax } from '@/components';
-import BasicElements from './components/BasicElementsSection';
-import Navigation from './components/Navigation';
-import TabsSection from './components/Tabs';
-import ProgressPagination from './components/ProgressPagination';
-import Notifications from './components/Notifications';
-import Typography from './components/Typography';
-import JavascriptComponents from './components/JavascriptComponents';
-import CarouselSection from './components/CarouselSection';
-import NucleoIconsSection from './components/NucleoIconsSection';
-import SignupForm from './components/SignupForm';
-import ExamplesSection from './components/ExamplesSection';
-import DownloadSection from './components/DownloadSection';
-import Plans from './components/Plans';
-import carousel from 'vue-owl-carousel'
+import { Parallax } from "@/components";
+import BasicElements from "./components/BasicElementsSection";
+import Navigation from "./components/Navigation";
+import TabsSection from "./components/Tabs";
+import ProgressPagination from "./components/ProgressPagination";
+import Notifications from "./components/Notifications";
+import Typography from "./components/Typography";
+import JavascriptComponents from "./components/JavascriptComponents";
+import CarouselSection from "./components/CarouselSection";
+import NucleoIconsSection from "./components/NucleoIconsSection";
+import SignupForm from "./components/SignupForm";
+import ExamplesSection from "./components/ExamplesSection";
+import DownloadSection from "./components/DownloadSection";
+import Plans from "./components/Plans";
+import carousel from "vue-owl-carousel";
 
 export default {
-  name: 'index',
+  name: "index",
   //data for typewriter
   data: () => {
     return {
       typeValue: "",
       typeStatus: false,
-      displayTextArray: ["Prevention","Detection", "Response"],
+      displayTextArray: ["Prevention", "Detection", "Response"],
       typingSpeed: 70,
       erasingSpeed: 70,
       newTextDelay: 700,
@@ -173,11 +219,14 @@ export default {
   },
   methods: {
     typeText() {
-      if (this.charIndex < this.displayTextArray[this.displayTextArrayIndex].length) {
+      if (
+        this.charIndex <
+        this.displayTextArray[this.displayTextArrayIndex].length
+      ) {
         if (!this.typeStatus) this.typeStatus = true;
-        this.typeValue += this.displayTextArray[this.displayTextArrayIndex].charAt(
-          this.charIndex
-        );
+        this.typeValue += this.displayTextArray[
+          this.displayTextArrayIndex
+        ].charAt(this.charIndex);
         this.charIndex += 1;
         setTimeout(this.typeText, this.typingSpeed);
       } else {
@@ -188,10 +237,9 @@ export default {
     eraseText() {
       if (this.charIndex > 0) {
         if (!this.typeStatus) this.typeStatus = true;
-        this.typeValue = this.displayTextArray[this.displayTextArrayIndex].substring(
-          0,
-          this.charIndex - 1
-        );
+        this.typeValue = this.displayTextArray[
+          this.displayTextArrayIndex
+        ].substring(0, this.charIndex - 1);
         this.charIndex -= 1;
         setTimeout(this.eraseText, this.erasingSpeed);
       } else {
@@ -204,7 +252,7 @@ export default {
     },
   },
   // till here typewriter
-  bodyClass: 'index-page',
+  bodyClass: "index-page",
   components: {
     // Parallax,
     // BasicElements,
@@ -220,14 +268,19 @@ export default {
     // ExamplesSection,
     // DownloadSection
     Plans,
-    carousel 
-  }
+    carousel,
+  },
 };
 </script>
 <style lang="scss" scoped>
-.typerclass {margin-bottom: 0px;font-weight: bold;}
+.typerclass {
+  margin-bottom: 0px;
+  font-weight: bold;
+}
 // all css for type writer
-.img{width: 150px}
+.img {
+  width: 150px;
+}
 .typer {
   width: 100%;
   font-size: 3rem;
--- src/pages/Soar.vue
+++ src/pages/Soar.vue
@@ -22,9 +22,9 @@
           Incidents are reported consistent with established criteria.
         </div>
         <div class="col-md-12 text-center">
-          <button type="button" class="btn btn-primary " @click="scroll()">
-              Show me more
-            </button>
+          <button type="button" class="btn btn-primary" @click="scroll()">
+            Show me more
+          </button>
         </div>
       </div>
     </div>
@@ -89,92 +89,103 @@
       </div> -->
       <div class="container" id="scroll">
         <div class="row">
-            <div class="col-md-4 mt-5">
-                <div class="card">
-                    <div class="circle">
-                      <i class="fa fa-wrench"></i>                    
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Workflows</h5>
-                        <p class="card-text">Workflows are made using a varitety of frameworks and tools.</p>
-                    </div>
-                </div>
+          <div class="col-md-4 mt-5">
+            <div class="card">
+              <div class="circle">
+                <i class="fa fa-wrench"></i>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title text-center">Workflows</h5>
+                <p class="card-text">
+                  Workflows are made using a varitety of frameworks and tools.
+                </p>
+              </div>
             </div>
-            <div class="col-md-4 mt-5">
-                <div class="card">
-                    <div class="circle circle1">
-                        <i class="fa fa-globe"></i>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Orchestration and Playbooks</h5>
-                        <p class="card-text">SOC platform crafts playbooks to orchestrate full response
-                chains.</p>
-                    </div>
-                </div>
+          </div>
+          <div class="col-md-4 mt-5">
+            <div class="card">
+              <div class="circle circle1">
+                <i class="fa fa-globe"></i>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title text-center">
+                  Orchestration and Playbooks
+                </h5>
+                <p class="card-text">
+                  SOC platform crafts playbooks to orchestrate full response
+                  chains.
+                </p>
+              </div>
             </div>
-            <div class="col-md-4 mt-5">
-                <div class="card">
-                    <div class="circle circle3">
-                        <i class="fa fa-laptop"></i>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Orchestrated Response</h5>
-                        <p class="card-text">Playbooks integrate different actions and targets.</p>
-                    </div>
-                </div>
+          </div>
+          <div class="col-md-4 mt-5">
+            <div class="card">
+              <div class="circle circle3">
+                <i class="fa fa-laptop"></i>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title text-center">Orchestrated Response</h5>
+                <p class="card-text">
+                  Playbooks integrate different actions and targets.
+                </p>
+              </div>
             </div>
+          </div>
         </div>
         <div class="row">
           <div class="col-md-4 mt-5">
-                <div class="card">
-                    <div class="circle circle4">
-                        <i class="fa fa-file"></i>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Artifacts Collection</h5>
-                        <p class="card-text">Playbooks execution can extend beyond the affected asset,
-                collecting additional artifacts.</p>
-                    </div>
-                </div>
+            <div class="card">
+              <div class="circle circle4">
+                <i class="fa fa-file"></i>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title text-center">Artifacts Collection</h5>
+                <p class="card-text">
+                  Playbooks execution can extend beyond the affected asset,
+                  collecting additional artifacts.
+                </p>
+              </div>
             </div>
-            <div class="col-md-4 mt-5">
-                <div class="card">
-                    <div class="circle circle5">
-                        <i class="fa fa-user"></i>
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title text-center">User Notifications</h5>
-                        <p class="card-text">Different communication channels available as part of the
-                orchestrated response.</p>
-                    </div>
-                </div>
+          </div>
+          <div class="col-md-4 mt-5">
+            <div class="card">
+              <div class="circle circle5">
+                <i class="fa fa-user"></i>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title text-center">User Notifications</h5>
+                <p class="card-text">
+                  Different communication channels available as part of the
+                  orchestrated response.
+                </p>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-      <div class="container text-center font-weight-bold">
-        <h2 style="color: blue;" >SOAR</h2>
-        <h3 >Security Orchestration, Automation amd Response.</h3>
       </div>
-      <!-- //hereeeeeeeeeeeeeeeeeeeeeeeeeeeee -->
-      <div class="accordion container">
+      <div class="container text-center font-weight-bold">
+        <h2 style="color: blue">SOAR</h2>
+        <h3>Security Orchestration, Automation amd Response.</h3>
+      </div>
+      <div class="accordion  container " id="accordionFlushExample">
         <div class="accordion-item">
-          <h2 class="accordion-header" id="headingOne">
+          <h2 class="accordion-header" id="flush-headingOne">
             <button
-              class="accordion-button"
+              class="accordion-button collapsed"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#collapseOne"
-              aria-expanded="true"
-              aria-controls="collapseOne"
+              data-bs-target="#flush-collapseOne"
+              aria-expanded="false"
+              aria-controls="flush-collapseOne"
             >
               Plugins and Integrations.
             </button>
           </h2>
           <div
-            id="collapseOne"
-            class="accordion-collapse collapse show"
-            aria-labelledby="headingOne"
-            data-bs-parent="#accordionExample"
+            id="flush-collapseOne"
+            class="accordion-collapse collapse"
+            aria-labelledby="flush-headingOne"
+            data-bs-parent="#accordionFlushExample"
           >
             <div class="accordion-body">
               <img class="imgshow" height="250" src="img/soar1.png" alt="" />
@@ -182,54 +193,59 @@
           </div>
         </div>
         <div class="accordion-item">
-          <h2 class="accordion-header" id="headingTwo">
+          <h2 class="accordion-header" id="flush-headingTwo">
             <button
               class="accordion-button collapsed"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#collapseTwo"
+              data-bs-target="#flush-collapseTwo"
               aria-expanded="false"
-              aria-controls="collapseTwo"
+              aria-controls="flush-collapseTwo"
             >
               Workflows.
             </button>
           </h2>
           <div
-            id="collapseTwo"
+            id="flush-collapseTwo"
             class="accordion-collapse collapse"
-            aria-labelledby="headingTwo"
-            data-bs-parent="#accordionExample"
+            aria-labelledby="flush-headingTwo"
+            data-bs-parent="#accordionFlushExample"
           >
             <div class="accordion-body">
-              <img class="imgshow" height="250" src="img/soar1.png" alt="" />
+              <img class="imgshow" height="250" src="img/soar2.png" alt="" />
             </div>
           </div>
         </div>
         <div class="accordion-item">
-          <h2 class="accordion-header" id="headingThree">
+          <h2 class="accordion-header" id="flush-headingThree">
             <button
               class="accordion-button collapsed"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#collapseThree"
+              data-bs-target="#flush-collapseThree"
               aria-expanded="false"
-              aria-controls="collapseThree"
+              aria-controls="flush-collapseThree"
             >
               Apps Ecosystem.
             </button>
           </h2>
           <div
-            id="collapseThree"
+            id="flush-collapseThree"
             class="accordion-collapse collapse"
-            aria-labelledby="headingThree"
-            data-bs-parent="#accordionExample"
+            aria-labelledby="flush-headingThree"
+            data-bs-parent="#accordionFlushExample"
           >
             <div class="accordion-body">
-              <img class="imgshow" height="250" src="img/soar1.png" alt="" />
+              <img class="imgshow" height="250" src="img/soar3.png" alt="" />
             </div>
           </div>
         </div>
       </div>
+      <div class="container">
+        <h4>Multiple apps out of the box and many more that we craft to cover all integration needs.</h4>
+      </div>
+      <br />
+      <br />
       <br />
     </div>
   </div>
@@ -302,7 +318,7 @@ export default {
   // till here typewriter
   bodyClass: "index-page",
 };
-import './Soar.css';
+import "./Soar.css";
 </script>
 
 <style lang="scss" scoped>
@@ -310,79 +326,9 @@ import './Soar.css';
   scroll-margin-top: 100px;
 }
 .imgshow {
-  width: 75%;
-  height: 450px;
+  width: 100%;
+  height: 500px;
   background-repeat: no-repeat;
   background-size: contain;
-  margin-left: 12%;
-  margin-bottom: 50px;
 }
-// h1 {
-//   margin-bottom: 0px;
-//   font-size: 15rem;
-//   font-weight: normal;
-//   span.typed-text {
-//     color: #d2b94b;
-//     // font-size: 60px;
-//   }
-// }
-// .card {
-//   margin-right: 20px !important;
-// }
-
-// // Cursor blinking CSS Starts...
-// .blinking-cursor {
-//   font-size: 3rem;
-//   color: #2c3e50;
-//   -webkit-animation: 1s blink step-end infinite;
-//   -moz-animation: 1s blink step-end infinite;
-//   -ms-animation: 1s blink step-end infinite;
-//   -o-animation: 1s blink step-end infinite;
-//   animation: 1s blink step-end infinite;
-// }
-// @keyframes blink {
-//   from,
-//   to {
-//     color: transparent;
-//   }
-//   50% {
-//     color: #2c3e50;
-//   }
-// }
-// @-moz-keyframes blink {
-//   from,
-//   to {
-//     color: transparent;
-//   }
-//   50% {
-//     color: #2c3e50;
-//   }
-// }
-// @-webkit-keyframes blink {
-//   from,
-//   to {
-//     color: transparent;
-//   }
-//   50% {
-//     color: #2c3e50;
-//   }
-// }
-// @-ms-keyframes blink {
-//   from,
-//   to {
-//     color: transparent;
-//   }
-//   50% {
-//     color: #2c3e50;
-//   }
-// }
-// @-o-keyframes blink {
-//   from,
-//   to {
-//     color: transparent;
-//   }
-//   50% {
-//     color: #2c3e50;
-//   }
-// }
 </style>
